d56491728d2be6c867dc49559be56f495a1362e6
jeff-niemann <>
2020-06-10 10:50:34 -0600

    Updates

---
+++ precache-manifest.15b66efcbfdf6a2a761fb6d80524257b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb59994a12d8d614c1a0",
+    "url": "/bbx/static/js/app.8a820e37.chunk.js"
+  },
+  {
+    "revision": "f45fbd2acf6aaf103057",
+    "url": "/bbx/static/js/runtime~app.9e08e500.js"
+  },
+  {
+    "revision": "ce2f25f5ddce9b91f949",
+    "url": "/bbx/static/js/2.692b0e25.chunk.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/bbx/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/bbx/expo-service-worker.js"
+  },
+  {
+    "revision": "46b47c0020bf1c8b654b4cd7419c73b1",
+    "url": "/bbx/index.html"
+  },
+  {
+    "revision": "c7e2e46f699c32032b2218b29e5a014e",
+    "url": "/bbx/manifest.json"
+  },
+  {
+    "revision": "4924d01f0ddc99d109b9b02e363d4311",
+    "url": "/bbx/register-service-worker.js"
+  }
+];
